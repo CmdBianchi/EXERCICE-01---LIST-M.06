693f22658eb2ca4fa326b0d5d01fa49b71256ddb
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-05-01 14:30:55 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCICE 01 - LIST M.06/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+namespace EXERCICE_01___LIST_M._06{
+    //-----> INICIO
+    ////////////////////////////////////////////////////////////////////////////////////////////-------------> IMPORTANTE 
+    class Program {
+        static void Main(string[] args) {
+            List<string> list = new List<string>();
+            List<string> list2 = new List<string> { "Maria", "Alex" };
+        }
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    //-----> FIM
+}
